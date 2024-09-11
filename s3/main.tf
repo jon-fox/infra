b9f2b7c1f794b5a136b7fec2a26e682873ecfa0d
jon fox <jon@foxsolutions.dev>
2024-09-10 22:25:14 -0500

    cloudfront distro

--- s3/main.tf
+++ s3/main.tf
@@ -104,3 +104,73 @@ resource "aws_iam_role_policy_attachment" "attach_s3_access_policy_lambda" {
   role       = data.aws_iam_role.existing_role_lambda.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
 }
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for accessing S3 bucket"
+}
+
+# Step 2: Update S3 Bucket Policy for CloudFront Access
+resource "aws_s3_bucket_policy" "app_storage_bucket_policy_cloudfront" {
+  bucket = aws_s3_bucket.app_storage_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::cloudfront:user/CloudFrontOriginAccessIdentity/${aws_cloudfront_origin_access_identity.oai.id}"
+        }
+        Action = "s3:GetObject"
+        Resource = [
+          "${aws_s3_bucket.app_storage_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Step 3: Create CloudFront Distribution
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.app_storage_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.app_storage_bucket.bucket}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.app_storage_bucket.bucket}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
