7f10e40c15cf2a1eaea5f8ca112e519d2d6e7a21
jon fox <jon@foxsolutions.dev>
2024-09-22 01:30:58 -0500

    ami change to ecs optimized

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -13,7 +13,8 @@ variable "instance_type" {
 variable "ami_id" {
   description = "AMI ID to use for the EC2 instances"
   # default     = "ami-0123456789abcdef0"
-  default     = "ami-075d39ebbca89ed55"
+  # default     = "ami-075d39ebbca89ed55"
+  default = "ami-0fffdbebbd264efe6" # ecs optimized
 }
 
   # terraform init -backend-config="bucket="
