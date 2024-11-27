c43e46311c57c88ea13dd142086e26e65933f484
jon fox <jon@foxsolutions.dev>
2024-11-27 16:51:17 -0600

    changing to standard sqs

--- sqs/main.tf
+++ sqs/main.tf
@@ -14,14 +14,16 @@ terraform {
 # }
 
 resource "aws_sqs_queue" "sqs_queue" {
-  name                      = "audio-processing-queue.fifo"
-  fifo_queue                = true
+  # name                      = "audio-processing-queue.fifo"
+  name                      = "audio-processing-queue"
+  # fifo_queue                = true
+  fifo_queue                = false
   visibility_timeout_seconds = 3600 # how long the message is invisible after it is being processed
   message_retention_seconds  = 86400 # retention
   delay_seconds              = 0 # delay of message visibility to consumers
   max_message_size           = 262144
   receive_wait_time_seconds  = 10 # how long sqs waits to return response, > 0 is long polling
-  content_based_deduplication = true # prevents dupe messages
+  # content_based_deduplication = true # prevents dupe messages
 }
 
 resource "aws_ssm_parameter" "sqs_queue_url" {
