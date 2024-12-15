8174e66e977877b5b96685121f1b304a666b146f
jon fox <jon@foxsolutions.dev>
2024-12-14 21:45:08 -0600

    sns changes

--- scaling-lambda/src/lambda_scaling_up.py
+++ scaling-lambda/src/lambda_scaling_up.py
@@ -12,12 +12,15 @@ SNS_TOPIC = ssm.get_parameter(Name="/sns/scaling_topic_arn")['Parameter']['Value
 MESSAGES_PER_INSTANCE = 10  # Customize based on processing capacity of each instance
 
 def notify_scaling_action(desired_capacity):
-    message = f"Scaling action taken. New desired capacity: {desired_capacity}"
-    sns.publish(
-        TopicArn=SNS_TOPIC,
-        Message=message,
-        Subject="Auto Scaling Notification"
-    )
+    try:
+        message = f"Scaling action taken. New desired capacity: {desired_capacity}"
+        sns.publish(
+            TopicArn=SNS_TOPIC,
+            Message=message,
+            Subject="Auto Scaling Notification"
+        )
+    except Exception as e:
+        print(f"Error sending SNS notification: {e}")
 
 def lambda_handler(event, context):
     # Retrieve the number of messages in the queue
@@ -38,21 +41,25 @@ def lambda_handler(event, context):
     current_capacity = asg['DesiredCapacity']
     min_size = asg['MinSize']
     max_size = asg['MaxSize']
+
+    print(f"Queue length: {queue_length}, Required instances: {required_instances}, Current capacity: {current_capacity}")
+    print(f"ASG min size: {min_size}, max size: {max_size}")
     
     # Ensure the desired capacity is within bounds
     desired_capacity = max(min_size, min(required_instances, max_size))
     
     # if no instances and queue length is greater than 1, start with 1 instance
     if current_capacity == 0 and queue_length > 1:
+        print(f"No instances available, Starting with 1 instance for queue length {queue_length}")
         desired_capacity = 1
 
     # Update the ASG if the capacity needs adjustment
     if desired_capacity != current_capacity:
+        print(f"Updating ASG desired capacity to {desired_capacity}")
         autoscaling.update_auto_scaling_group(
             AutoScalingGroupName=AUTO_SCALING_GROUP,
             DesiredCapacity=desired_capacity
         )
-        print(f"Updated ASG desired capacity to {desired_capacity}")
         notify_scaling_action(desired_capacity)
     else:
         print("No scaling action required")
