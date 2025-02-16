79637f644d319bd7b3aa6d8fa3b93ced5d6c9b6f
jon fox <jon@foxsolutions.dev>
2025-02-16 00:48:56 -0600

    update url paths

--- lambda_edge/src/index.py
+++ lambda_edge/src/index.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     request = event['Records'][0]['cf']['request']
     uri = request['uri']
 
-    # Redirect any .html URL to its equivalent without .html
+    # Remove .html from URLs (redirect users)
     if uri.endswith(".html"):
         return {
             "status": "301",
@@ -12,9 +12,13 @@ def lambda_handler(event, context):
             "headers": {
                 "location": [{
                     "key": "Location",
-                    "value": uri[:-5]  # Removes '.html' without adding a slash
+                    "value": uri[:-5]  # Removes .html
                 }]
             }
         }
+
+    # Ensure S3 receives a valid file request
+    if not uri.endswith("/") and "." not in uri:
+        request.uri += ".html"  # Append .html for S3 compatibility
 
     return request
