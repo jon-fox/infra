b4b1589cc5d3560d287c1e90180b48a9281cebd9
jon fox <jon@foxsolutions.dev>
2025-02-16 00:33:38 -0600

    updating role

--- lambda_edge/main.tf
+++ lambda_edge/main.tf
@@ -9,13 +9,29 @@ terraform {
   }
 }
 
-data "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
+resource "aws_iam_role" "lambda_edge_role" {
+  name = "lambda_edge_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = [
+            "lambda.amazonaws.com",        
+            "edgelambda.amazonaws.com"     # Lambda@Edge
+          ]
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_policy_attachment" "lambda_edge_policy" {
   name       = "lambda_edge_policy"
-  roles      = [data.aws_iam_role.lambda_role.name]
+  roles      = [aws_iam_role.lambda_edge_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -25,7 +41,7 @@ data "aws_ssm_parameter" "cloudfront_distribution_id" {
 
 resource "aws_lambda_function" "redirect_index" {
   function_name    = "redirect-html"
-  role             = data.aws_iam_role.lambda_role.arn
+  role             = aws_iam_role.lambda_edge_role.arn
   handler          = "index.lambda_handler"
   runtime          = "python3.12"
   filename         = "lambda_edge.zip"
